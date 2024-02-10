1f0d53931a61d349d243f8e64ae587d71e651e5a
John-Davidson-8 <jodo828@gmail.com>
2024-02-10 19:31:07 +0000

    Edit form

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-// Is this the "submit listener" (no click listeners) needed for the JS?
 
-const email = document.getElementById("email");
-
-email.addEventListener("input", (event) => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("Please provide a valid email address!");
-  } else {
-    email.setCustomValidity("");
-  }
-});
