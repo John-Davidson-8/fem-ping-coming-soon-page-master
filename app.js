edbf4f451ad4c6a71a24e2a28f5d34bf65fab2ee
John-Davidson-8 <jodo828@gmail.com>
2024-02-11 19:00:25 +0000

    Add JS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const email = document.getElementById("mail");
 
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 email.addEventListener("input", (event) => {
   if (email.validity.typeMismatch) {
     email.setCustomValidity("Please provide a valid email address!");
